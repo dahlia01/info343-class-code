a69b05b421f91cc0b0c2f0ceb7ab153cf447a31f
Dahlia Sidhu <dahliasidhu@hotmail.com>
2015-10-13 17:22:07 -0700

    code from class

--- javascript/js/index.js
+++ javascript/js/index.js
@@ -1,3 +1,32 @@
 /**
  * application script for index.html
  */
+
+document.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+
+    function forEachElement(collection, fn) {
+        var idx;
+        for (idx = 0; idx < collection.length; idx++) {
+            fn(collection[idx]);
+        }
+    }
+
+    var clickMeButton = document.getElementById('click-me');
+    clickMeButton.addEventListener('click', function() {
+        var alerts = document.querySelectorAll('.alert');
+        forEachElement(alerts, function(a) {
+            a.style.display = 'block';
+
+        });
+    });
+
+    var closeButtons = document.querySelectorAll('.alert .close');  //get array (or array like structure, a 'node list') of objects that match the selector
+    forEachElement(closeButtons, function(button) {
+        button.addEventListener('click', function() {
+            button.parentElement.style.display = 'none';
+        });
+    });
+});
+
+
--- javascript/js/states.js
+++ javascript/js/states.js
@@ -1,3 +1,32 @@
 /**
  * application script for the states.html
  */
+
+document.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+
+    function addStates(states, listElem) {
+        listElem.innerHTML = '';
+        states.forEach(function(state) {
+            //console.log(state);
+
+            var li = document.createElement('li');
+            li.textContent = state.name; // sets the internal content, not attached to document yet
+            listElem.appendChild(li); // append li element to ul
+        });
+    }
+
+    var statesFilter = document.getElementById('state-filter-field');
+    statesFilter.addEventListener('keyup', function() {
+       var filter = this.value.toLowerCase();
+       //console.log(filter);
+        var filteredStates = usaStates.filter(function(state) {
+           return state.name.toLowerCase().indexOf(filter) >= 0;
+        });
+        addStates(filteredStates, statesUl);
+    });
+
+    var statesUl = document.getElementById('states-list');
+    addStates(usaStates, statesUl); // get the ul element with states-list id
+
+});
