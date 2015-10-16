d80d2d3eaaaf14ba2ac607d6f2d6dbabac7d1923
Dahlia <dahliasidhu@hotmail.com>
2015-10-15 17:13:16 -0700

    code from class

--- jquery/js/app.js
+++ jquery/js/app.js
@@ -6,3 +6,25 @@
     http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f
 */
 
+/*Jquery version of DOMContentLoaded is $(function(){});
+
+$(function() {
+    'use strict';
+    $('a').attr('target', '_blank')
+    $('article').hide().fadeIn(1000); // method chaining: every jquery function returns the object that the function was called on
+    $('#toggle-article').click(function() {
+        $('article').fadeToggle();
+    });
+
+    // returns object called promise
+    // this is an asynchronous call, our code will continue to run while the data is being received from server
+    var url = 'http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f';
+    $.getJSON(url)
+        .then(function(data) { //.then waits for the getJSON to get all data from server
+            console.log(data);
+            var temperature = data.main.temp;
+            $('#temp').text(Math.round(temperature));
+    });
+
+    console.log('test'); // Note test is printed to console before the data because the data hasn't been recieved yet (getJSON is asynchronous)
+});
--- leaflet/js/app.js
+++ leaflet/js/app.js
@@ -8,3 +8,37 @@
     attribution:
     '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 */
+
+$(function() {
+    'use strict';
+
+    function createMap(loc, zoom) {
+        var map = L.map('map').setView(loc, zoom); // L.map() takes the id of element that you want to contain the map.
+
+
+        //Note: google maps returns longetude west. To make this work with mapping software, it needs a negative sign in front.
+        // tilelayer sits on top of map object, map provides zooming effects
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { //template url to fetch tiles from - leaflet library will replace {z}/{x}/{y}
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        // mapping software returns small tiles so that when you pan and scroll they can get the tiles for that region (vs grabbing a gigantic image)
+
+        L.circleMarker(loc).addTo(map).bindPopup('<h2>Hello</h2>');
+
+    }
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            createMap([pos.coords.latitude, pos.coords.longitude], 15);
+        });
+    } else {
+        createMap([47.6097, -122.3331], 12)
+    }
+
+
+    // make sure you handle the error code (success & error)  - error is when user hits 'block' instead of 'allow'
+});
+
+
+
