b0ea3e391554f97c64964090fae44ab356b6f55e
Dahlia <sidhud@uw.edu>
2015-11-05 16:59:22 -0800

    this code kinda doesn't work

--- firebase/js/app.js
+++ firebase/js/app.js
@@ -1,6 +1,25 @@
 
 angular.module('ChatApp', ['firebase'])
     .constant('firebaseUrl', 'https://info343chat.firebaseio.com/messages')
-    .controller('ChatController', function($scope) {
-        //TODO: implement our chat controller
+    .controller('ChatController', function($scope, $firebaseArray,firebaseUrl) {
+        //create reference to the Firebase
+        var ref = new Firebase(firebaseUrl);
+        ref.limitToLast(1000);
+        $scope.messages = $firebaseArray(ref);
+
+        // initialize form fields
+        $scope.name = null;
+        $scope.body = null;
+
+        $scope.sendMessages = function() {
+            //adds a new object ot the array and synchronizes
+            // with the server
+            $scope.messages.$add({
+                name: $scope.name,
+                body: $scope.body,
+                createdAt: Firebase.ServerValue.TIMESTAMP
+            });
+
+            $scope.body = null;
+        }; //sendMessage()
     });
