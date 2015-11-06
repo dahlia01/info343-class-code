2ee7af2a66dcf23ee62c6c784440ef9279b0c2e5
Dahlia <sidhud@uw.edu>
2015-11-05 16:35:41 -0800

    11/5 angular-forms

--- angular-forms/js/app.js
+++ angular-forms/js/app.js
@@ -5,13 +5,7 @@
 angular.module('ContactsApp', ['ui.router', 'angular-uuid', 'LocalStorageModule'])
     .constant('storageKey', 'contacts-list')
     .factory('contacts', function(uuid, localStorageService, storageKey) { //factory only gets executed once, gets injected into any controller that asks for it
-        return [{
-            id: 'default-delete-me',
-            fname: 'Fred',
-            lname: 'Flintstone',
-            phone: '206-555-1211',
-            dob: '1/1/1900'
-        }];
+        return localStorageService.get(storageKey) || [];
     })
     .config(function($stateProvider, $urlRouterProvider){
         $stateProvider
@@ -33,6 +27,17 @@ angular.module('ContactsApp', ['ui.router', 'angular-uuid', 'LocalStorageModule'
         $urlRouterProvider.otherwise('/contacts'); // url will always reset to end with /contacts unless it fits one of the paths defined above
 
     })
+    .directive('inThePast', function() { // method on the module that allows you to declare a new directive
+        return {
+            require : 'ngModel',
+            link: function(scope, elem, attrs, controller) {
+                controller.$validators.inThePast = function(modelValue) {
+                    var today = new Date();
+                    return (new Date(modelValue) <= today);
+                }
+            }
+        };
+    })
     .controller('ContactsController', function($scope, contacts) { // anuglar will set parameter contacts to whatever the factory 'contacts' returns
         $scope.contacts = contacts;
     })
@@ -41,7 +46,7 @@ angular.module('ContactsApp', ['ui.router', 'angular-uuid', 'LocalStorageModule'
            return contact.id === $stateParams.id; //capture the property after : in the local url
         });
     })
-    .controller('EditContactController', function($scope, $stateParams, $state, contacts) {
+    .controller('EditContactController', function($scope, $stateParams, $state, contacts, uuid, localStorageService, storageKey, contacts) {
         var existingContact = contacts.find(function(contact) {
             return contact.id === $stateParams.id; //capture the property after : in the local url
             // return the contact for which the above expression evaluates to true
@@ -50,7 +55,15 @@ angular.module('ContactsApp', ['ui.router', 'angular-uuid', 'LocalStorageModule'
         $scope.contact = angular.copy(existingContact); //so that we don't edit the actual contact - this allows cancel to work
 
         $scope.save = function() {
-            angular.copy($scope.contact, existingContact);
+            if($scope.contact.id) {
+                angular.copy($scope.contact, existingContact);
+            } else {
+                $scope.contact.id = uuid.v4(); //generate a version 4 universal unique id
+                contacts.push($scope.contact);
+            }
+
+            localStorageService.set(storageKey, contacts);
+
             $state.go('list');
         }
     });
