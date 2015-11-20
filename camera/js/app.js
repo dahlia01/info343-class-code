d6cc7ae196452f422f98c16067a506d93a93dd6b
dahlia <sidhud@uw.edu>
2015-11-19 17:06:12 -0800

    11/19 class code

--- camera/js/app.js
+++ camera/js/app.js
@@ -2,6 +2,64 @@
 document.addEventListener('DOMContentLoaded', function() {
     'use strict';
 
+    navigator.getUserMedia = navigator.getUserMedia
+                            || navigator.webkitGetUserMedia
+                            || navigator.mozGetUserMedia
+                            || navigator.msGetUserMedia;
+
+    var video = document.querySelector('video');
+    var canvas = document.querySelector('canvas');
+    var ctx = canvas.getContext('2d');
+    var videoStream;
+    var mouseIsDown = false;
+
+    navigator.getUserMedia({video: true}, function(stream) {
+        //dynamic object that returns constant stream of video images
+        videoStream = stream;
+        video.src = window.URL.createObjectURL(stream);
+
+    }, function(err) {
+        console.error(err);
+    });
+
+    video.addEventListener('click', function() {
+        if(videoStream) {
+            canvas.width = video.clientWidth;
+            canvas.height = video.clientHeight;
+            ctx.drawImage(video, 0, 0);
+        }
+    });
+
+    canvas.addEventListener('mousedown', function(evt) {
+        console.log('mousedown');
+        var x = evt.clientX - canvas.offsetLeft;
+        var y = evt.clientY - canvas.offsetTop + window.scrollY;
+        ctx.strokeStyle = 'yellow';
+        ctx.lineWidth= 5;
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        mouseIsDown = true;
+    });
+
+    canvas.addEventListener('mousemove', function(evt) {
+        var x = evt.clientX - canvas.offsetLeft;
+        var y = evt.clientY - canvas.offsetTop + window.scrollY;
+
+        if (mouseIsDown) {
+            ctx.lineTo(x, y);
+            ctx.stroke();
+            console.log(mouseIsDown);
+        }
+    });
+
+    canvas.addEventListener('mouseup', function(evt) {
+        mouseIsDown = false;
+    });
+
+    document.querySelector('#btnSnapShot').addEventListener('click', function() {
+        console.log('snapshot');
+        document.querySelector('img').src = canvas.toDataURL();
+    });
 
 });
 
--- notifications/js/app.js
+++ notifications/js/app.js
@@ -3,4 +3,45 @@
 document.addEventListener('DOMContentLoaded', function() {
     'use strict';
 
+    function askPermission() {
+        Notification.requestPermission(function(result) {
+            if ('granted' === result) {
+                showNotification('Thanks!', 'You will now see my notifications')
+            }
+        });
+    }
+
+    function showNotification(title, body) {
+        var note = new Notification(title, {
+            body: body,
+            icon: 'img/notification.png'
+        });
+
+        function dismissAlert() {
+            note.close();
+        }
+
+        window.setTimeout(note.close.bind(note), 3000);
+        // a function is an object, one of the methods is {} which is to call the function itself,
+        // bind is another function on the function object,
+        // it binds the current note object to the close function
+        // this tells it which note to close (in the case that you have multiple notifications open at one time)
+
+    }
+
+    var triggerBtn = document.getElementById('trigger');
+    triggerBtn.addEventListener('click', function() {
+        switch (Notification.permission) {
+            case 'granted':
+                showNotification('Hello', 'triggered at ' + new Date().toLocaleTimeString());
+                break;
+            case 'denied':
+                alert('Please enable notifications!');
+                break;
+            default:
+                askPermission();
+        }
+    });
 });
+
+
